e202c5c67df5bafbee413570e87860f984ffff89
PuedoYT <86661140+PuedoYT@users.noreply.github.com>
2023-12-18 19:50:55 +0100

    Update MainApp.java
    finished some stuff

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,3 +1,4 @@
+import java.sql.SQLException;
 import java.util.List;
 
 public class MainApp {
@@ -5,7 +6,16 @@ public class MainApp {
     private static DatabaseService databaseService = new DatabaseService();
     private static DataProcessor dataProcessor = new DataProcessor();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        MariaDB db = new MariaDB();
+        
+        db.setDatabaseLink("jdbc:mariadb://localhost");
+        db.setDatabasePort(3306);
+        db.setUsername("root");
+        db.setPassword("ROOT");
+        db.setDatabaseName("test");
+
+        db.connect();
         List<String> fetchedData = databaseService.fetchData("puedo", "test");
         System.out.println(dataProcessor.processData(fetchedData));
     }
